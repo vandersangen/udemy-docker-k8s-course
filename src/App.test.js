2dee3f78fc74fe3b6dff395fa3a6c3d1565219d5
Lars van der Sangen <lars@vandersangen.dev>
2023-05-17 17:15:18 +0200

    omg

--- src/App.test.js
+++ src/App.test.js
@@ -3,7 +3,7 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Learn how to React/i);
   expect(linkElement).toBeInTheDocument();
 });
 
